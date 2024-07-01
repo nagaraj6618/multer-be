10fc5701556759873d608cc8dd613a408c0d1fd9
NAGARAJ S <nagaraj516700@gmail.com>
2024-07-01 14:57:41 +0530

    Vercel.json added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const multer = require("multer");
 require('dotenv').config();
 const PORT = process.env.PORT;
 
@@ -18,6 +19,35 @@ app.get('/',(req,res) => {
       success:true
    });
 
+});
+
+
+const storage = multer.diskStorage({
+   destination:function (req,file,cb){
+      cb(null,"./uploads");
+   },
+   filename:function(req,file,cb){
+      cb(null,file.originalname);
+   }
+});
+
+const uploads = multer({storage:storage});
+
+app.post('/',uploads.single('image'),async(req,res) => {
+
+   const image = req.file;
+   console.log(image);
+   if(!image){
+      return res.status(500).json({
+         message:"File upload is not successfull",
+         success:false,
+      });
+
+   };
+   console.log(image);
+   res.status(200).json({
+      message:"Success true",
+   })
 })
 
 app.listen(PORT,()=>console.log(`http://localhost:${PORT}`));
